ed7d75be8b3d4a83d6ffb40e7ad5fb4a2a46c7ac
Devraj Bhagvat <bhagvatdevraj@gmail.com>
2026-05-08 17:54:53 +0530

    added the struct part

---
+++ bank.h
@@ -0,0 +1,6 @@
+struct Account {
+    int accNo;        // whole number
+    char name[50];    // text (string in C = array of characters)
+    char type[10];    // "Savings" or "Current"
+    float balance;    // decimal number
+};
