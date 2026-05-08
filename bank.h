0a82b60b6d9b8ba55d332ce45924f96bd892ab69
Devraj Bhagvat <bhagvatdevraj@gmail.com>
2026-05-08 17:57:11 +0530

    Added strut in bank.h

--- bank.h
+++ bank.h
@@ -1,6 +1,20 @@
+#ifndef BANK_H
+#define BANK_H
+
 struct Account {
-    int accNo;        // whole number
-    char name[50];    // text (string in C = array of characters)
-    char type[10];    // "Savings" or "Current"
-    float balance;    // decimal number
+    int accNo;
+    char name[50];
+    char type[15];
+    float balance;
 };
+
+void createAccount();
+void viewAllAccounts();
+void depositMoney();
+void withdrawMoney();
+void searchByAccNo();
+void searchByName();
+void updateAccount();
+void deleteAccount();
+
+#endif
